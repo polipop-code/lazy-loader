ccf521d2c29c18cf6a89567274dfccebb509ba0a
Jesus Politron <polipop.work@gmail.com>
2022-05-29 01:03:31 -0500

    updated and cleanup the project

--- index.js
+++ index.js
@@ -13,45 +13,45 @@ const randomWord = () => Math.floor(Math.random() * wordArray.length);
 const random = () => Math.floor(Math.random() * (max - min)) + min;
 
 const createImageNode = () => {
-	// Div - Container
-	const container = document.createElement("div");
-	container.className = "foxContainer";
+    // Div - Container
+    const container = document.createElement("div");
+    container.className = "foxContainer";
 
-	// H2 - Title
-	const h2 = document.createElement("h2");
-	h2.textContent = `A ${wordArray[randomWord()]} fox has appear`;
+    // H2 - Title
+    const h2 = document.createElement("h2");
+    h2.textContent = `A ${wordArray[randomWord()]} fox has appear`;
 
-	// Image - Content
-	const image = document.createElement("img");
-	// image.src = "https://source.unsplash.com/random/?{coffee,cup}";
-	image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+    // Image - Content
+    const image = document.createElement("img");
+    // image.src = "https://source.unsplash.com/random/?{coffee,cup}";
+    image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-	// P - Description
-	const p = document.createElement("p");
-	p.textContent = "Delivery for you, a Fox :D";
+    // P - Description
+    const p = document.createElement("p");
+    p.textContent = "Delivery for you, a Fox :D";
 
-	container.append(h2, image, p);
-	return container;
+    container.append(h2, image, p);
+    return container;
 };
 
 // Add button
 const addImage = () => {
-	const newImage = createImageNode();
-	reference.appendChild(newImage);
-	registerImage(newImage);
-	totalFox.innerText = accountant.total;
+    const newImage = createImageNode();
+    reference.appendChild(newImage);
+    registerImage(newImage);
+    totalFox.innerText = accountant.total;
 };
 
 // Limpiar
 const removeImage = () => {
-	// reference.remove("div.foxContainer");
-	while (reference.firstChild) {
-		reference.removeChild(reference.firstChild);
-	}
-	console.clear();
-	accountant.loaded = 0;
-	accountant.total = 0;
-	totalFox.innerText = accountant.total;
+    // reference.remove("div.foxContainer");
+    while (reference.firstChild) {
+        reference.removeChild(reference.firstChild);
+    }
+    console.clear();
+    accountant.loaded = 0;
+    accountant.total = 0;
+    totalFox.innerText = accountant.total;
 };
 
 document.querySelector("button").addEventListener("click", addImage);
--- lazyLoader.js
+++ lazyLoader.js
@@ -2,41 +2,41 @@ let sightedFox = document.querySelector("p.sightedFox");
 
 // accountant.loaded
 let accountant = {
-	total: 0,
-	loaded: 0,
+    total: 0,
+    loaded: 0,
 };
 // let accountant.loaded = 0;
 // let accountant.total = 0;
 
 // Intersection observer
 const isIntersecting = (entry) => {
-	return entry.isIntersecting;
+    return entry.isIntersecting;
 };
 
 const loadImage = (entry) => {
-	const node = entry.target;
+    const node = entry.target;
 
-	console.log("🟣 Imágenes cargadas: " + (accountant.loaded + 1) + " de " + accountant.total);
-	accountant.loaded += 1;
-	sightedFox.innerText = accountant.loaded;
+    console.log("🟣 Imágenes cargadas: " + (accountant.loaded + 1) + " de " + accountant.total);
+    accountant.loaded += 1;
+    sightedFox.innerText = accountant.loaded;
 
-	const image = node.querySelector("img");
+    const image = node.querySelector("img");
 
-	const url = image.dataset.src;
-	image.src = url;
+    const url = image.dataset.src;
+    image.src = url;
 
-	observer.unobserve(node);
+    observer.unobserve(node);
 };
 
 const observer = new IntersectionObserver((allEntries) => {
-	allEntries.filter(isIntersecting).forEach(loadImage);
+    allEntries.filter(isIntersecting).forEach(loadImage);
 });
 
 // Lazy Loader
 const registerImage = (image) => {
-	observer.observe(image);
-	console.log("⚪ Total de Imágenes: " + (accountant.total + 1));
-	accountant.total += 1;
+    observer.observe(image);
+    console.log("⚪ Total de Imágenes: " + (accountant.total + 1));
+    accountant.total += 1;
 };
 
 export { registerImage, accountant };
